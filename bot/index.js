134b9c8ba9dc1a6dc81f6bc78d06e148c2dbde76
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-12 18:23:34 +0100

    added owner-only command

--- bot/index.js
+++ bot/index.js
@@ -7,12 +7,15 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
 	if (message.content.startsWith(config.prefix + "test")) {
 		message.channel.send("Quasar is working.");
 	} else if (message.content.startsWith(config.prefix + "ban esti")) {
 		message.channel.send("**Esteriore** is no longer in the server.");
+	} else if (message.content.startsWith(config.prefix + "clear")) {
+		if (message.author.id !== config.ownerID) return;
+		message.channel.send("I can't do that yet.");
 	}
 });
 
