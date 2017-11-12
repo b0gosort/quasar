572b858236d6b6bedae4e41bbf1baa2e503b40de
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-12 17:28:56 +0100

    added ban esti and message filter

--- bot/index.js
+++ bot/index.js
@@ -1,14 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("../config.json");
 
 client.on("ready", () => {
 	console.log("Quasar is ready!");
 });
 
 client.on("message", (message) => {
-	if (message.content.startsWith("quasar test")) {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	if (message.content.startsWith(config.prefix + "test")) {
 		message.channel.send("Quasar is working.");
+	}
+
+	if (message.content.startsWith(config.prefix + "ban esti")) {
+		message.channel.send("**Esteriore** is no longer in the server.");
 	}
 });
 
