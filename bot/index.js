0a4b2cb75f805f167b66ff2a6df93dd2c7beeb4e
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-12 21:25:52 +0100

    switched to argument structure, added echo

--- bot/index.js
+++ bot/index.js
@@ -9,13 +9,27 @@ client.on("ready", () => {
 client.on("message", (message) => {
 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-	if (message.content.startsWith(config.prefix + "test")) {
-		message.channel.send("Quasar is working.");
-	} else if (message.content.startsWith(config.prefix + "ban esti")) {
-		message.channel.send("**Esteriore** is no longer in the server.");
-	} else if (message.content.startsWith(config.prefix + "clear")) {
-		if (message.author.id !== config.ownerID) return;
-		message.channel.send("I can't do that yet.");
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === "test") {
+		message.channel.send("Quasar is operational.");
+	} else if (command === "ban") {
+		let target = args[0];
+		if (target == null) {
+			message.channel.send("No target was specified.");
+		} else if (target.toLowerCase() === "esti") {
+			message.channel.send("**Esteriore** is no longer in the server.")
+		} else {
+			if (message.author.id !== config.ownerID) {
+				message.channel.send("You don't have permission to use the command ``ban``.");
+				return;
+			}
+			message.channel.send("Sorry, but Quasar doesn't yet have that feature.");
+		}
+	} else if (command === "echo") {
+		let text = args.slice(0).join(" ");
+		message.channel.send(text);
 	}
 });
 
