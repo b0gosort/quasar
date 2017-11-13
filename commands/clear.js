f4e3c5fcda6e2e78edb75e578a166f7456296de3
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-13 12:12:11 +0100

    added clear command

---
+++ commands/clear.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args, config) => {
+	if (!args || args.length < 1) return message.channel.send("One or more arguments were missing.");
+
+	if (message.author.id !== config.ownerID) return message.channel.send("You don't have permission to use the command ``clear``.");
+
+	let numMessages = args[0];
+	if (numMessages < 2) return message.channel.send("The number provided was too low.");
+	message.channel.bulkDelete(parseInt(numMessages) + 1);
+	message.channel.send(`${numMessages} messages from the past 2 weeks have been deleted.`);
+}
