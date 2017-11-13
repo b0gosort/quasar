eabef2b3672efbd9e4d82c382786a00160edc555
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-13 10:22:46 +0100

    added command does not exist message

--- bot/index.js
+++ bot/index.js
@@ -5,12 +5,14 @@ const fs = require("fs");
 
 const config = require("./config.json");
 
+const commandFiles = fs.readdirSync("./commands/");
+
 client.on("ready", () => {
 	console.log("Quasar is ready!");
 });
 
 client.on("guildMemberAdd", (member) => {
-	member.guild.channels.get(config.joinLog).send(`${member.user.username} has joined the server.`);
+	member.guild.channels.get(config.joinLog).send(`**${member.user.username}** has joined the server.`);
 });
 
 client.on("message", (message) => {
@@ -19,6 +21,7 @@ client.on("message", (message) => {
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
+	if (commandFiles.indexOf(command + ".js") === -1) return message.channel.send("The command ``" + command + "`` does not exist.");
 	try {
 		let commandFile = require(`./commands/${command}.js`);
 		commandFile.run(client, message, args);
