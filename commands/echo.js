2b0c685850c8812afbe54350b60eb1137736c9ed
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-13 14:41:39 +0100

    echo is admin only and deletes original message

--- commands/echo.js
+++ commands/echo.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args, config) => {
+	if (message.author.id !== config.ownerID) return message.channel.send("You don't have permission to use ``echo``.");
+
 	if (!args || args.length < 1) return message.channel.send("One or more arguments were missing.");
 
 	let text = args.slice(0).join(" ");
 	message.channel.send(text);
+	message.delete();
 }
