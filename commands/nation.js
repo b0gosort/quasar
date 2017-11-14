86617d419385b5f6c00969071ff8b59cacf64131
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-14 18:21:46 +0100

    nation command uses embed

--- commands/nation.js
+++ commands/nation.js
@@ -4,8 +4,8 @@ exports.run = (client, message, args, config) => {
 
 	if (!args || args.length < 1) return message.channel.send("One or more arguments were missing");
 
-	let target = args.join(" ");
-	let targetURL = `https://www.nationstates.net/cgi-bin/api.cgi?nation=${target}&q=wa+region+lastactivity+category+population+name+fullname`;
+	let target = args.join("_").toLowerCase();
+	let targetURL = `https://www.nationstates.net/cgi-bin/api.cgi?nation=${target}&q=wa+region+lastactivity+category+fullname+flag`;
 
 	request({
 		url: targetURL,
@@ -15,21 +15,33 @@ exports.run = (client, message, args, config) => {
 		console.log("Error: " + error);
 		console.log("Status code: " + response);
 
-		//message.channel.send("```xml\n" + body + "\n```");
+		if (body.indexOf("Error") !== -1) return message.channel.send("There was an error with your request.");
 
 		parseString(body, function (error, result) {
 			console.log("Parse error: " + error);
 
 			let data = result.NATION;
-			let type = data.FULLNAME[0].substring(0, data.FULLNAME[0].indexOf(data.NAME[0]) - 1);
-			message.channel.send(
-				`**__${data.NAME[0]}__**, ${type}\n` +
-				`**Category**: ${data.CATEGORY[0]}\n` +
-				`**Population**: ${data.POPULATION[0]} million \n` +
-				`**Region**: ${data.REGION[0]}\n` +
-				`**WA Status**: ${data.UNSTATUS[0]}\n` +
-				`**Active**: ${data.LASTACTIVITY[0]}`
-			);
+			let embed = {
+				color: 0x1772c9,
+				title: data.FULLNAME[0],
+				description: data.CATEGORY[0],
+				url: `https://nationstates.net/nation=${target}`,
+				thumbnail: {url: data.FLAG[0]},
+				fields: [{
+					name: "Region",
+					value: data.REGION[0]
+				},
+				{
+					name: "WA Status",
+					value: data.UNSTATUS[0]
+				},
+				{
+					name: "Active",
+					value: data.LASTACTIVITY[0]
+				}]
+			};
+			
+			message.channel.send({embed});
 		});
 	});
 }
