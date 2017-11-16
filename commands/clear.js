45e553273a9a4bdad7b17b9890d2d18c2cebcc5f
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-16 10:03:22 +0100

    changed formatting of commands in perm denied msgs

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args, config) => {
 	if (!args || args.length < 1) return message.channel.send("One or more arguments were missing.");
 
-	if (message.author.id !== config.ownerID) return message.channel.send("You don't have permission to use the command ``clear``.");
+	if (message.author.id !== config.ownerID) return message.channel.send("You don't have permission to use the command **clear**.");
 
 	let numMessages = args[0];
 	if (numMessages < 2) return message.channel.send("The number provided was too low.");
