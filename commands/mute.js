f90c27cefd5b433f4def7d10b09c22be3deadd01
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-17 18:52:54 +0100

    added mute

---
+++ commands/mute.js
@@ -0,0 +1,17 @@
+exports.run = function(client, message, args, config) {
+	if (config.admins.indexOf(message.author.id) === -1) return message.channel.send("You don't have permission to use the command **mute**.");
+
+	if (!args || args.length < 1) return message.channel.send("One or more arguments were missing.");
+
+	let target = message.mentions.members.first();
+
+	message.channel.overwritePermissions(target, {
+		"SEND_MESSAGES": false,
+		"ATTACH_FILES": false
+	}).then(function() {
+		return message.channel.send(`**${target.user.username}** is now muted.`);
+	}).catch(function(error) {
+		console.log(error);
+		return message.channel.send("There was an error when attempting to mute that member.");
+	});
+}
