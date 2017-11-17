42f51776ac8cf1c0523c2d5c658ec1eb2c71434f
Cooper Johnston <sverige.cooper@gmail.com>
2017-11-17 17:24:07 +0100

    register changes username to nation name

--- commands/register.js
+++ commands/register.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args, config) => {
 
 	let target = message.author;
 	let nation = args.join("_").toLowerCase();
-	let nationURL = `https://www.nationstates.net/cgi-bin/api.cgi?nation=${nation}&q=wa+region`;
+	let nationURL = `https://www.nationstates.net/cgi-bin/api.cgi?nation=${nation}&q=wa+region+name`;
 
 	request({
 		url: nationURL,
@@ -31,7 +31,7 @@ exports.run = (client, message, args, config) => {
 
 				message.member.addRole(foreignRole).catch(console.error);
 				client.users.get(config.ownerID).send(`**${target.username}** was assigned the role **${config.roles.foreign}** with the nation **${nation}**.`);
-				return message.channel.send(`You have been assigned the role **${config.roles.foreign}**. If you are here as a diplomat, please send a message to a government member.`);
+				message.channel.send(`You have been assigned the role **${config.roles.foreign}**. If you are here as a diplomat, please send a message to a government member.`);
 			} else if (data.UNSTATUS[0] === "WA Member") {
 				//For WA members in the region
 
@@ -39,12 +39,14 @@ exports.run = (client, message, args, config) => {
 
 				message.member.addRole(citizenRole).catch(console.error);
 				client.users.get(config.ownerID).send(`**${target.username}** was assigned the role **${config.roles.citizen}** with the nation **${nation}**.`);
-				return message.channel.send(`You have been assigned the role **${config.roles.citizen}**. Please endorse the WA Delegate if you haven't already.`);
+				message.channel.send(`You have been assigned the role **${config.roles.citizen}**. Please endorse the WA Delegate if you haven't already.`);
 			} else {
 				//For non-WA members in the region
 
 				return message.channel.send("Please join the World Assembly (https://nationstates.net/page=un) to gain citizenship and try again.");
 			}
+
+			return message.member.edit({nick: data.NAME[0]}).catch(function(error) {console.log(error)});
 		});
 	});
 }
