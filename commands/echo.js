b1f2d263f3eac49dd023e378541b73e838353026
ModzOn <modzongaming@gmail.com>
2017-11-23 19:09:52 +0100

    ensure they can't ping things using the command

--- commands/echo.js
+++ commands/echo.js
@@ -1,11 +1,12 @@
+const { cleanContent } = require("../functions");
 exports.run = (client, message, args, config) => {
 	if (config.admins.indexOf(message.author.id) === -1) return message.channel.send("You don't have permission to use the command **echo**.");
 
 	if (!args.length) return message.channel.send("One or more arguments were missing.");
 
-	let text = args.join(" ");
+	const text = args.join(" ");
 	message.delete();
-	return message.channel.send(text);
+	return message.channel.send(cleanContent(message, text));
 };
 
 exports.info = {
