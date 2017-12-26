21506b14696b6d0d06415320e19566b1a339053d
Michel Nguyen <modzongaming@gmail.com>
2017-12-26 12:43:36 +0100

    make help more efficient

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,15 @@
 const { stripIndents } = require("common-tags");
-exports.run = (client, message, args) => { // eslint-disable-line consistent-return
+exports.run = (client, message, args) => {
 	let messages = [];
-	if (!args || args.length < 1) {
-		messages.push("**Quasar Discord Bot** by Solborg\n```");
-
-		message.author.send(stripIndents`
+	if (!args.length) {
+		messages.push(message.author.send(stripIndents`
 			**Quasar Discord Bot** by Solborg
 			\`\`\`
       ${client.commands.map(command => `${command.info.name}: ${command.info.desc}`).join("\n")}
 			\`\`\`
-		`).then(() => { // eslint-disable-line
-			return message.channel.send("A list of commands was delivered to your direct messages.");
-		}).catch(() => { // eslint-disable-line
+		`));
+		messages.push(message.channel.send("A list of commands was delivered to your direct messages."));
+		return Promise.all(messages).catch(() => { // eslint-disable-line
 			return message.channel.send("The list of commands could not be delivered to you. Make sure you have direct messages enabled.");
 		});
 	} else {
