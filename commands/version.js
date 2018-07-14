fc1be018eb6b77d367aa9a6d434dc7603e9907a4
Cooper <b0gosort@users.noreply.github.com>
2018-07-13 22:03:23 -0700

    updated the version command

--- commands/version.js
+++ commands/version.js
@@ -1,3 +1,3 @@
 exports.run = function(client, message, args, config) {
-	message.channel.send("Quasar Release 1.1.1 (December 2017) is currently running.");
+	message.channel.send("Quasar Release 1.1.2 (July 2018) is currently running.");
 }
